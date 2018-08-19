5749df2016b5c4497e350bc9848fd62167e569bc
Chris Chalmers <chrischalmers.cpc@gmail.com>
2018-08-18 20:36:17 -0700

    First try at migrating IRTransmitter to pic16f18877

--- LaserTag.X/IRTransmitter.c
+++ LaserTag.X/IRTransmitter.c
@@ -1,4 +1,3 @@
-#include <xc.h>
 #include "IRTransmitter.h"
 
 #include "bitwiseUtils.h"
@@ -6,107 +5,168 @@
 #include "transmissionConstants.h"
 
 #include <stdbool.h>
+#include <xc.h>
 
-#define TRANSMIT_PIN P1MDLBIT
+/*
+ * HOW IT WORKS
+ *
+ * There are a whopping five modules involved in transmitting, compared to the
+ * two used in past iterations. We may remove modules in the future if we find
+ * them to be too much of a maintenance or power consumption cost. The advantage
+ * of using all these modules is that it makes the output timing precise enough
+ * to be used with a high-frequency carrier wave in the future.
+ *
+ * The output is ultimately produced by the Data Signal Modulator (DSM) module.
+ * The DSM combines a carrier signal with a modulation signal. The carrier
+ * carrier signal is a 40kHz PWM signal. When the modulation signal is HIGH, the
+ * carrier wave is fed to the output. When the modulation signal is LOW, the
+ * output is low.
+ *
+ * The carrier signal is produced by a PWM module, which uses an 8-bit timer.
+ *
+ * The modulation signal is produced by a CCP module in Compare mode. The CCP
+ * module uses a 16-bit timer. It is configured to toggle the output, clear the
+ * timer, and trigger an interrupt when the 16-bit CCPR register matches the
+ * 16-bit timer register. In the interrupt handler we set CCPR for the next
+ * transition. We assume the interrupt handler will run before the timer reaches
+ * an applicable CCPR value.
+ *
+ * To start a transmission, we clear the timer and set CCPR to 0 to immediately
+ * trigger the Compare event.
+ *
+ * TODO look into using the PWM itself as the modulation signal timer source,
+ * e.g. TxCKIPPS. Try setting PWM6OUT and T4IN to the same pin and setting timer
+ * 4 clock source to TxCKIPPS.
+ */
 
-static void configureTimer2()
+static void configureTimer6()
 {
-    // Timer 2 is used to time P1MDLBIT switches
-    
-    //           +------ T2OUTPS[3:0] - Set output postscaler 1:1
-    //           | +---- TMR2ON       - Turn TMR2 off for now, we'll turn it on when we shoot
-    //           | | +-- T2CKPS[1:0]  - Set prescaler 1:16
-    //         |--||||
-    T2CON = 0b00000010;
-    TMR2IE = 1;
+    // Timer 6 is used by PWM6, which is used to construct the modulated high
+    // carrier signal
+    // Set clock source to Fosc (32MHz)
+    T6CLKCON = 0b0010;
+    //        +------- Turn TMR6 off for now, we'll turn it on when we transmit
+    //        | +----- Set prescaler to 1:1
+    //        | |   +- Set postscaler to 1:1
+    //        ||-||--|
+    T6CON = 0b00000000;
 
-    TRANSMIT_PIN = LOW;
+    // Set timer 6 period such that the PWM frequency (f) is 40kHz
+    // T6PR = -1 + Fosc / (f * 4 * prescale)
+    //      = -1 + 32MHz / (40kHz * 4 * 1)
+    //      = 199
+    T6PR = 0b11000111;
 }
 
-static void configurePSMC(void)
+static void configurePWM6(void)
 {
-    // Set up PSMC1 for modulated fixed duty cycle PWM
-    // Disable the PSMC module before configuring it
-    PSMC1CON = 0;
-    //           +-------- P1MDLEN     - Enable modulation
-    //           |     +-- P1MSRC[3:0] - Select P1MDLBIT as the modulation source
-    //           |   |--|
-    PSMC1MDL = 0b10000000;
-    //              +----- P1CPRE[1:0] - Set clock prescaler to 1:1
-    //              |   +- P1CSRC[1:0] - Select PSMC1 clock source: 64MHz internal
-    //             ||  ||
-    PSMC1CLK = 0b00000001;
-    //                  +- P1PRST - Set period event to occur when PSMC1TMR == PSMC1PR
-    //                  |
-    PSMC1PRS = 0b00000001;
-    // Set period register to 799. F = (64MHz / (PSMC1PR + 1)) / 2 = 40KHz
-    PSMC1PR = 0b0000001100011111;
-    //                  +- P1OEA - Enable PWM on PSMC1A
-    //                  |
-    PSMC1OEN = 0b00000001;
+    // Set PWM6 timer source to TMR6
+    CCPTMRS1bits.P6TSEL = 0b11;
+    // Set duty cycle to half of the Timer 6 period (200 / 2 = 100)
+    PWM6DCH = 0b1100100;
+    PWM6DCL = 0b00;
+}
 
-    //           +-------- PSMC1EN     - Enable PSMC1
-    //           |     +-- P1MODE[3:0] - Select fixed duty cycle, variable frequency, single PWM
-    //           |   |--|
-    PSMC1CON = 0b10001010;
+static void configureTimer5(void)
+{
+    // Set prescaler to 1:1
+    T5CONbits.CKPS = 0b00;
+    // Turn off TMR5 for now. We'll turn it on when we shoot
+    T5CONbits.ON = 0;
+    // Set TMR5 clock source as HFINTOSC (32MHz)
+    T5CLK = 0b0011;
+}
+
+static void configureCCP5(void)
+{
+    // Set CCP5 to Compare. Toggle output on match, and clear TMR5
+    CCP5CONbits.CCP5MODE = 0b0001;
+    // Set CCP5 timer source to TMR5
+    CCPTMRS1bits.C5TSEL = 0b11;
+}
+
+#define DSM_OUT_PPS 0x1B
+#define CCP5_OUT_PPS 0x0D
+
+#define A1_IN_PPS 0x01
+
+static void configureDSM(void)
+{
+    MDEN = 1;
+    // Synchronize output with the high carrier signal (the modulated one)
+    MDCHSYNC = 1;
+    // Set the modulation source to the external MDMSRC pin, whose location is
+    // configured by MDSRCPPS
+    MDSRC = 0b00000;
+    // Set carrier high signal source to PWM6_out
+    MDCARH = 0b1001;
+    // Set carrier low signal source to Vss. There doesn't seem to be a way to
+    // do this properly, so for now we're using the output of the NCO module,
+    // because it should be unused. To be safe, we're disabling the module too.
+    // TODO look into why we can't set the source to Vss
+    MDCARL = 0b1011;
+    NCOMD = 1;
+
+    // Select A1 as the modulation source pin
+    MDSRCPPS = A1_IN_PPS;
+    TRISA1 = 1; // A1 to input
+
+    // Select A0 as the output pin for the DSM
+    RA0PPS = DSM_OUT_PPS;
+    TRISA0 = 0; // A0 to output
 }
 
 void initializeTransmitter()
 {
-    configureTimer2();
-    configurePSMC();
-    TRISCbits.TRISC0 = 0; // C0 to output
+    // DSM uses PWM6 and CCP5. PWM6 uses TMR6. CCP5 uses TMR4
+    configureTimer6();
+    configurePWM6();
+    configureTimer5();
+    configureCCP5();
+    configureDSM();
 }
 
-// TMR2 clocks at (Fosc / 4) and is prescaled 16x. 32MHz / 4 / 16 = 0.5MHz
-#define TMR2_FREQ 500000 // 0.5MHz
-// TMR2 period in microseconds. Assumes period is a whole integer
-#define TMR2_PERIOD_US (1000000 / (TMR2_FREQ))
+// TMR5 clocks at 32MHz (HFINTOSC) and is prescaled 1x
+#define TMR5_FREQ 32000000
+
+// Ratio of TMR5 clock frequency to the carrier signal modulation frequency.
+// Assumes they divide evenly
+#define TMR5_MOD_CLOCK_RATIO ((TMR5_FREQ) / (MODULATION_FREQ))
 
 /*
- * With the PSMC in modulation mode, turning off a pulse causes the pulse to
- * complete its current cycle and then stop. Turning it on again immediately
- * starts a new cycle.
+ * The DSM output is synchronized to the high carrier signal, which means
+ * modulation line changes are reflected in the following cycle of the high
+ * carrier signal rather than immediately. Both the rising and falling edges of
+ * the modulation signal are synchronized.
  *
- * Keeping the output enabled for the length of a 10-cycle pulse will produce an
- * 11-cycle pulse, because the 11th cycle starts before we disable the output.
- * To output a 10-cycle pulse, we should instead output for slightly longer than
- * the length of 9 cycles, so that the 10th cycle begins before we turn off the
- * output.
- *
- * We start timing the gap as soon as we stop the previous pulse, so we need to
- * add the adjustment back to the gap time to make up for stopping the pulse a
- * little early.
+ * Because the PWM module runs independently of the DSM module, we don't know
+ * how the carrier signal and the modulation signal are phase-shifted relative
+ * to each other. This may make it difficult to output a precise number of
+ * pulses. TODO investigate.
  */
 
-// Define adjustment as 90% of the modulation period. I.e. make state changes
-// 10% of the way through the cycle preceding the targeted one.
-// Careful, We're fudging with integers here
-#define TRANSMITTER_TIMING_ADJUSTMENT_US ((MODULATION_PERIOD_US) - ((MODULATION_PERIOD_US) / 10))
-
-// Pulse lengths in terms of TMR2 cycles, adjusted for output
-#define PULSE_GAP_LENGTH_TMR2_CYCLES  (((PULSE_GAP_LENGTH_US) + (TRANSMITTER_TIMING_ADJUSTMENT_US)) / (TMR2_PERIOD_US))
-#define ZERO_PULSE_LENGTH_TMR2_CYCLES (((ZERO_PULSE_LENGTH_US) - (TRANSMITTER_TIMING_ADJUSTMENT_US)) / (TMR2_PERIOD_US))
-#define ONE_PULSE_LENGTH_TMR2_CYCLES  (((ONE_PULSE_LENGTH_US) - (TRANSMITTER_TIMING_ADJUSTMENT_US)) / (TMR2_PERIOD_US))
+// Pulse lengths in terms of TMR5 cycles
+#define PULSE_GAP_LENGTH_TMR5_CYCLES ((PULSE_GAP_LENGTH_MOD_CYCLES) * (TMR5_MOD_CLOCK_RATIO))
+#define ZERO_PULSE_LENGTH_TMR5_CYCLES ((ZERO_PULSE_LENGTH_MOD_CYCLES) * (TMR5_MOD_CLOCK_RATIO))
+#define ONE_PULSE_LENGTH_TMR5_CYCLES ((ONE_PULSE_LENGTH_MOD_CYCLES) * (TMR5_MOD_CLOCK_RATIO))
 
 #ifdef EVALUATE_CONSTANTS
 #include <stdint.h>
-const uint32_t TMR2_FREQ_eval = TMR2_FREQ;
-const uint8_t TMR2_PERIOD_US_eval = TMR2_PERIOD_US;
-const uint16_t TRANSMITTER_TIMING_ADJUSTMENT_US_eval = TRANSMITTER_TIMING_ADJUSTMENT_US;
+const uint32_t TMR5_FREQ_eval = TMR5_FREQ;
+const uint16_t TMR5_MOD_CLOCK_RATIO_eval = TMR5_MOD_CLOCK_RATIO;
 const uint16_t PULSE_GAP_LENGTH_TMR2_CYCLES_eval = PULSE_GAP_LENGTH_TMR2_CYCLES;
 const uint16_t ZERO_PULSE_LENGTH_TMR2_CYCLES_eval = ZERO_PULSE_LENGTH_TMR2_CYCLES;
 const uint16_t ONE_PULSE_LENGTH_TMR2_CYCLES_eval = ONE_PULSE_LENGTH_TMR2_CYCLES;
 #endif
 
-typedef uint8_t TMR2_t;
+typedef uint16_t TMR5_t;
 
 static volatile bool g_transmitting = false;
 static uint16_t g_transmission_data = 0;
 
 void handleTransmissionTimingInterrupt()
 {
-    if (!(TMR2IF && TMR2IE))
+    if (!(CCP5IF && CCP5IE))
         return;
 
     // Send MSB first
@@ -115,25 +175,35 @@ void handleTransmissionTimingInterrupt()
 
     if (next_edge_rising)
     {
-        TRANSMIT_PIN = HIGH;
+        // Output is now HIGH
+
         next_edge_rising = false;
         transmission_data_index--;
-        
-        TMR2_t pulse_width =
-                ((g_transmission_data >> transmission_data_index) & 1) ? 
-                    ONE_PULSE_LENGTH_TMR2_CYCLES : ZERO_PULSE_LENGTH_TMR2_CYCLES;
-        PR2 = pulse_width;
+
+        TMR5_t pulse_width =
+                ((g_transmission_data >> transmission_data_index) & 1) ?
+                    ONE_PULSE_LENGTH_TMR5_CYCLES : ZERO_PULSE_LENGTH_TMR5_CYCLES;
+        CCPR5 = pulse_width;
     }
     else // !next_edge_rising
     {
-        TRANSMIT_PIN = LOW;
+        // Output is now LOW
+
         next_edge_rising = true;
 
         if (transmission_data_index == 0)
         {
             // This is the final falling edge of the shot
-            // Turn off the timer
-            TMR2ON = 0;
+
+            // Disable CCP
+            CCP5EN = 0;
+            // Turn off CCP timer
+            TMR5ON = 0;
+            // Disable PWM
+            PWM6EN = 0;
+            // Disable PWM timer
+            TMR6ON = 0;
+
             // Reset the data index
             transmission_data_index = TRANSMISSION_LENGTH;
             // Let the main program know we're done transmitting
@@ -141,12 +211,12 @@ void handleTransmissionTimingInterrupt()
         }
         else
         {
-            PR2 = PULSE_GAP_LENGTH_TMR2_CYCLES;
+            CCPR5 = PULSE_GAP_LENGTH_TMR5_CYCLES;
         }
     }
 
     // Reset flag
-    TMR2IF = 0;
+    CCP5IF = 0;
 }
 
 bool transmitAsync(uint16_t data)
@@ -154,16 +224,24 @@ bool transmitAsync(uint16_t data)
     if (g_transmitting)
         return false;
     g_transmitting = true;
-    
+
     // Append parity bits to the transmission
     uint8_t bit_sum = sumBits(data);
     uint8_t parity_bits = bit_sum & ((1 << NUM_PARITY_BITS) - 1);
     g_transmission_data = (data << NUM_PARITY_BITS) | parity_bits;
-    
-    TMR2 = 0;
-    TMR2ON = 1;
-    // Force the interrupt handler to run to start the shot process
-    TMR2IF = 1;
-    
+
+    // Clear and enable PWM timer
+    TMR6 = 0;
+    TMR6ON = 1;
+    // Enable PWM
+    PWM6EN = 1;
+    // Set compare target to 0 to immediately capture
+    CCPR5 = 0;
+    // Clear and enable CCP timer
+    TMR5 = 0;
+    TMR5ON = 1;
+    // Enable CCP5
+    CCP5EN = 1;
+
     return true;
 }
