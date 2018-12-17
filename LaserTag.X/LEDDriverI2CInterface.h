98fb2dc63701c186f519d31dab3a8be99377c0d5
Chris Chalmers <chrischalmers.cpc@gmail.com>
2018-12-16 22:46:56 -0800

    <BREAKING> Initial transition to PCB, including LED driver

---
+++ LaserTag.X/LEDDriverI2CInterface.h
@@ -0,0 +1,10 @@
+#ifndef LEDDISPLAY_H
+#define	LEDDISPLAY_H
+
+#include <stdint.h>
+
+void initializeLEDDisplay(void);
+// LSB is the first LED. 1 is on, 0 is off.
+void setLEDDisplay(uint16_t bits);
+
+#endif	/* LEDDISPLAY_H */
